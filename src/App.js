ac53f1e90a7c7feedfbe455ac9010c1e57ff5383
Chandra-sinha1999 <54039460+Chandra-sinha1999@users.noreply.github.com>
2021-08-26 19:45:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import {useState,useEffect} from 'react';
 import DisplayData from './DisplayData';
 
-
 function App() {
-  const [isLoading,setIsLoading] = useState(true);
-  const [apiData,setApiData] = useState([]);
+  const [data,setData] = useState([]);
 
   useEffect(() => {
-    fetch(
-      'https://mocki.io/v1/18e2400b-4cb6-4531-970c-79cf0ef63bd8'
-      )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let apiArray = [];
-      apiArray = data["data"];
-      const newArray = [];
-
-      for(const key in apiArray) {
-        const curData = {
-          id: key,
-          ...apiArray[key]
-        };
-        newArray.push(curData);
-      }
-
-      setIsLoading(false);
-      setApiData(newArray);
-    });
+    fetch( 'https://mocki.io/v1/18e2400b-4cb6-4531-970c-79cf0ef63bd8')
+    .then((response) => { return response.json(); })
+    .then(recivedData => setData(recivedData.data) );
   }, []);
-
-  if(isLoading){
-    return(
-      <section>
-        <p>Loading</p>
-      </section>
-    );
-  }
 
   return (
     <section>
-        <DisplayData record={apiData}/>
+        <DisplayData record={data}/>
     </section>
-);
+  );
 }
 
 export default App;
+
