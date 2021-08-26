9c1068872229c0cf84a05bc1fbf6b377b86cc9a1
Chandra-sinha1999 <54039460+Chandra-sinha1999@users.noreply.github.com>
2021-08-26 19:47:58 +0530

    Update DisplayData.js

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -1,20 +1,32 @@
 import DisplayItem from "./DisplayItem";
 
 function DisplayData(props) {
-    //console.log(props.record);
-    return (
-        <ul className="card">
-            {props.record.map((singleRecord) => (
-                <DisplayItem 
-                    key = {singleRecord.id}
-                    icon = {singleRecord.icon}
-                    title = {singleRecord.title}
-                    description = {singleRecord.desc}
-                    timeframe = {singleRecord.timeframe}
-                />
-            ))}
-        </ul>
-    );
+    
+    function loadingHandler() {
+        return(
+            <section>
+              <p>Loading</p>
+            </section>
+        );
+    }
+
+    function dataHandler() {
+        return (
+            <ul className="card">
+                {props.record.map((singleRecord) => (
+                    <DisplayItem 
+                        key = {singleRecord.section}
+                        icon = {singleRecord.icon}
+                        title = {singleRecord.title}
+                        description = {singleRecord.desc}
+                        time = {singleRecord.timeframe}
+                    />
+                ))}
+            </ul>
+        );
+    }
+
+    return (props.record.length === 0)? loadingHandler() : dataHandler();
 }
 
 export default DisplayData;
